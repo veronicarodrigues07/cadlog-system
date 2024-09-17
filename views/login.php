4c7a0b8db849025d82335d6f58e5fd215ae8ce20
veronicarodrigues07 <veronicaizabellyrodrigues12@gmail.com>
2024-09-17 11:10:13 -0300

    Adiconando códigos para Email, senha e Login no vs

---
+++ views/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=z, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <form action= ""method="post">
+            <section>
+            <label for="email"> Email</label>
+                <input type="email" name="email" id="email" required >
+            </section>
+            <section>
+            <label for="senha"></label>
+            <input type="password" name="senha" id="senha">
+            </section>
+            <button type="submit">Login</button>
+        </form>
+            <a href=""></a>
+        
+    </main>
+</body>
+</html>
