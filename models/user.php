f9f9ab48c33a3b4887646d97972d1e2a16f2504e
veronicarodrigues07 <veronicaizabellyrodrigues12@gmail.com>
2024-09-18 08:31:20 -0300

    conecta a conexão com o BD

--- models/user.php
+++ models/user.php
@@ -1,5 +1,24 @@
 <?php
 
     //inclui um arquivo de conexão que contem a classe Database para gerenciar a conexão com o BD
-    require_once  'models/database.php'
+    require_once  'models/database.php';
+
+    class User
+    {
+        //Função para encontrar um usuário pelo email
+        public static function findByEmail($email){
+            //conecta a conexão com o BD
+            $conn = Database::getConnection();
+
+            $stmt = $conn ->prepare("SELECT * FROM usuarios WHERE email = :email");
+
+            $stmt -> execute(['email' => $email]);
+
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+
+        }
+
+    } 
 ?>
